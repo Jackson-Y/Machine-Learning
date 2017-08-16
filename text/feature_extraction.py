f1004efdece43dca465ffc9aca046d4ec7c6c6d9
moxiao <827297635@qq.com>
2017-08-16 13:48:16 +0800

    文本特征提取

--- text/feature_extraction.py
+++ text/feature_extraction.py
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*-
 '''
 Description:
-    Extract the feature from the text.
+    Extract the feature from the text in English.
 Version:
     python3
 '''
 from sklearn.feature_extraction.text import CountVectorizer
 
 VECTORIZER = CountVectorizer(min_df=1)
+
+# 以下代码设置了特征提取方法的参数（以1-2个单词作为滑动窗口大小，以空格作为单词的分割点，最小词频为1）
+# 详细参考API介绍: 
+# http://scikit-learn.org/stable/modules/feature_extraction.html#text-feature-extraction
+# VECTORIZER = CountVectorizer(ngram_range=(1,2), token_pattern=r'\b\w+\b', min_df=1)
 
 CORPUS = [
     'This is the first document.',
@@ -20,4 +25,3 @@ X = VECTORIZER.fit_transform(CORPUS)
 FEATURE_NAMES = VECTORIZER.get_feature_names()
 
 print(FEATURE_NAMES)
-print(X.toarray())
