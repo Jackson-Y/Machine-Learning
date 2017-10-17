15120c2d5f1e7a955fc0f894dfb4ee7c65e88246
moxiao <827297635@qq.com>
2017-10-17 10:56:51 +0800

    基于TensorFlow平台的线性回归模型 -- MNIST数据集

---
+++ MNIST/liner_softmax_mnist.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+""" 基于TF平台的线性softmax回归模型 -- MNIST数据集 """
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+sess = tf.InteractiveSession()
+x = tf.placeholder("float", shape=[None, 784])
+y_ = tf.placeholder("float", shape=[None, 10])
+
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+
+sess.run(tf.global_variables_initializer())
+
+y = tf.nn.softmax(tf.matmul(x, W) + b)
+cross_entropy = -tf.reduce_sum(y_*tf.log(y))
+train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+
+for i in range(1000):
+    batch = mnist.train.next_batch(50)
+    train_step.run(feed_dict={x: batch[0], y_: batch[1]})
+    
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+print(accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
